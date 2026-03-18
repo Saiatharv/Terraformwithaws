63af72660163c5266070e3f85d955ce82341b6bd
Saiatharv <karthik.suresh.chemnitzer@gmail.com>
2026-03-18 11:35:38 +0100

    Add DigitalOcean provider and update AWS configuration

--- challenge1/module1.tf
+++ challenge1/module1.tf
@@ -4,23 +4,30 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+   digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
   }
 }
 
 
-
-
 provider "aws" {
   region  = "us-east-1"
-  access_key = "AKIAYC7SGCH5MOVAQX7Q"
-  secret_key = "rN1jpxeL21r6MUDURUF/5E3gNsz7sGz2awvDNiqb"
+  }
+
+
+provider "digitalocean" {
+  token = var.do_token
 }
 
-
-provider "digitalocean" {}
-
 terraform {
-    required_version = "6.36.0"
+    required_version = ">= 1.5"
+}
+
+resource "aws_eip" "kplabs_app_ip" {
+  vpc = true
 }
 
 
